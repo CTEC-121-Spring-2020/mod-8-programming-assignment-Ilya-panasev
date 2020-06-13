3f4b8684432770e92172d89f3adf653ce7fc6139
Ilya <i.panasevich@students.clark.edu>
2020-06-12 17:57:42 -0700

    Unsure whether problem 2 is correct

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,4 +2,63 @@
 #   Programming Assignment 11
 #     Prob-1.py
 
-# Your code below
+class Car:
+    # constructor
+    def __init__(self, make, model, year):
+        self._make = make
+        self._model = model
+        self._year = year
+
+    def setMake(self, make):
+        self._make = make
+
+    def getMake(self):
+        return self._make
+
+    def setModel(self, model):
+        self._model = model
+    
+    def getModel(self):
+        return self._model
+    
+    def setYear(self, year):
+        self._year = year
+
+    def getYear(self):
+        return self._year
+
+def TestCar():
+    carLot = []
+    carLot.append(Car("minubishi", "achord", "2003"))
+    carLot.append(Car("farfella", "camora", "2017"))
+    carLot.append(Car("fiorde", "automoten", "2010"))
+    carLot.append(Car("kyota", "samazo", "2013"))
+    carLot.append(Car("landza", "arratil", "2006"))
+    
+    i = 1
+    for car in carLot:
+        print("car #",i)
+        print("Make:", car.getMake())
+        print("Model:", car.getModel())
+        print("Year:", car.getYear(), "\n")
+        i = i + 1
+    
+    i = 1
+    for car in carLot:
+        Make = input("Enter a car make: ")
+        Model = input("Enter a car model: ")
+        Year = input("Enter the year of the car: ")
+        car.setMake(Make)
+        car.setModel(Model)
+        car.setYear(Year)
+
+    i = 1
+    for car in carLot:
+        print("car #",i)
+        print("Make:", car.getMake())
+        print("Model:", car.getModel())
+        print("Year:", car.getYear(), "\n")
+        i = i + 1
+
+if __name__ == "__main__":
+    TestCar()
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,4 +2,66 @@
 #   Programming Assignment 11
 #     Prob-2.py
 
-# Your code below
+class Student:
+    # constructor
+    # Whenever I go up to Gaiser hall to register for classes or go to an advisor
+    # they usually ask me what my name is first, then ask me for my student ID
+    # in order to pull up the rest of my information. Major and GPA are usually 
+    # listed under the information they pull up.
+    def __init__(self, name, IDnumber):
+        self._name = name
+        self._IDnumber = IDnumber
+        self._major = ""
+        self._GPA = 0.0
+
+    def setName(self, name):
+        self._name = name
+
+    def getName(self):
+        return self._name
+
+    def setIDnumber(self, IDnumber):
+        self._IDnumber = IDnumber
+
+    def getIDnumber(self):
+        return self._IDnumber
+    
+    def setMajor(self, major):
+        self._major = major
+
+    def getMajor(self):
+        return self._major
+
+    def setGPA(self, GPA):
+        self._GPA = GPA
+    
+    def getGPA(self):
+        return self._GPA
+
+def TestStudent():
+    stuList = []
+    stuList.append(Student("Joe Bella", "9933"))
+    stuList.append(Student("Tucker Blank", "3399"))
+    stuList.append(Student("Gayle Ujifusa", "1011"))
+    stuList.append(Student("Edna Anker", "9875"))
+    
+    stuList[0].setMajor("Web Development")
+    stuList[1].setMajor("Nursing")
+    stuList[2].setMajor("Baking")
+    stuList[3].setMajor("Medical Office")
+
+    stuList[0].setGPA(3.8)
+    stuList[1].setGPA(3.0)
+    stuList[2].setGPA(2.8)
+    stuList[3].setGPA(3.0)
+
+    for element in stuList:
+        print("Name:", element.getName())
+        print("ID Number:", element.getIDnumber())
+        print("Major:", element.getMajor())
+        print("GPA:", element.getGPA())
+        print()
+        
+
+if __name__ == "__main__":
+    TestStudent()
